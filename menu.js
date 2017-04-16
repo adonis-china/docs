ad7596c1ee18b9527adf7cf5a789a92c72898be0
xuesong <wu-xuesong@qq.com>
2017-04-16 12:57:33 +0800

    init

---
+++ menu.js
@@ -0,0 +1,439 @@
+/**
+ * Created by xuesong on 2017/4/15.
+ */
+module.exports = {
+  "web开发工具": [
+    {
+      "permalink": "interactive-shell",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "交互式shell Aka Ace",
+      "weight": 0
+    },
+    {
+      "permalink": "authentication",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "认证",
+      "weight": 0
+    },
+    {
+      "permalink": "cookies",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "Cookies",
+      "weight": 0
+    },
+    {
+      "permalink": "mail",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "Mail",
+      "weight": 0
+    },
+    {
+      "permalink": "error-and-exceptions",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "错误和异常处理",
+      "weight": 0
+    },
+    {
+      "permalink": "events",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "事件",
+      "weight": 0
+    },
+    {
+      "permalink": "helpers",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "助手",
+      "weight": 0
+    },
+    {
+      "permalink": "sessions",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "Sessions",
+      "weight": 0
+    },
+    {
+      "permalink": "validator",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "验证",
+      "weight": 0
+    },
+    {
+      "permalink": "redis",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "Redis",
+      "weight": 0
+    },
+    {
+      "permalink": "internationalization",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "国际化",
+      "weight": 0
+    },
+    {
+      "permalink": "social-auth",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "基于Ally的社交登陆",
+      "weight": 0
+    },
+    {
+      "permalink": "websocket",
+      "categories": [
+        "web开发工具"
+      ],
+      "title": "WebSocket",
+      "weight": 0
+    }
+  ],
+  "核心理论": [
+    {
+      "permalink": "mvc-pattern",
+      "categories": [
+        "核心理论"
+      ],
+      "title": "MVC模式",
+      "weight": 0
+    },
+    {
+      "permalink": "ioc-container",
+      "categories": [
+        "核心理论"
+      ],
+      "title": "IoC容器 & Service Providers",
+      "weight": 0
+    },
+    {
+      "permalink": "app-lifecycle",
+      "categories": [
+        "核心理论"
+      ],
+      "title": "生命周期",
+      "weight": 0
+    }
+  ],
+  "数据库": [
+    {
+      "permalink": "database-setup",
+      "categories": [
+        "数据库"
+      ],
+      "title": "数据库设置",
+      "weight": 0
+    },
+    {
+      "permalink": "query-builder",
+      "categories": [
+        "数据库"
+      ],
+      "title": "Query Builder",
+      "weight": 0
+    },
+    {
+      "permalink": "migrations",
+      "categories": [
+        "数据库"
+      ],
+      "title": "迁移",
+      "weight": 0
+    },
+    {
+      "permalink": "seeds-and-factories",
+      "categories": [
+        "数据库"
+      ],
+      "title": "填充和工厂",
+      "weight": 0
+    }
+  ],
+  "起步": [
+    {
+      "permalink": "installation",
+      "categories": [
+        "起步"
+      ],
+      "title": "安装",
+      "weight": 0
+    },
+    {
+      "permalink": "directory-structure",
+      "categories": [
+        "起步"
+      ],
+      "title": "目录结构",
+      "weight": 0
+    },
+    {
+      "permalink": "config",
+      "categories": [
+        "起步"
+      ],
+      "title": "配置",
+      "weight": 0
+    },
+    {
+      "permalink": "env",
+      "categories": [
+        "起步"
+      ],
+      "title": "环境变量",
+      "weight": 0
+    },
+    {
+      "permalink": "routing",
+      "categories": [
+        "起步"
+      ],
+      "title": "路由",
+      "weight": 0
+    },
+    {
+      "permalink": "request",
+      "categories": [
+        "起步"
+      ],
+      "title": "请求",
+      "weight": 0
+    },
+    {
+      "permalink": "response",
+      "categories": [
+        "起步"
+      ],
+      "title": "响应",
+      "weight": 0
+    },
+    {
+      "permalink": "middleware",
+      "categories": [
+        "起步"
+      ],
+      "title": "中间件",
+      "weight": 0
+    },
+    {
+      "permalink": "controllers",
+      "categories": [
+        "起步"
+      ],
+      "title": "控制器",
+      "weight": 0
+    },
+    {
+      "permalink": "files",
+      "categories": [
+        "起步"
+      ],
+      "title": "文件",
+      "weight": 0
+    }
+  ],
+  "lucid": [
+    {
+      "permalink": "lucid",
+      "categories": [
+        "lucid"
+      ],
+      "title": "Lucid",
+      "weight": 0
+    },
+    {
+      "permalink": "relationships",
+      "categories": [
+        "lucid"
+      ],
+      "title": "关联",
+      "weight": 0
+    },
+    {
+      "permalink": "database-hooks",
+      "categories": [
+        "lucid"
+      ],
+      "title": "数据库Hooks",
+      "weight": 0
+    },
+    {
+      "permalink": "database-getters-setters",
+      "categories": [
+        "lucid"
+      ],
+      "title": "Getters & Setters",
+      "weight": 0
+    }
+  ],
+  "序言": [
+    {
+      "permalink": "overview",
+      "categories": [
+        "序言"
+      ],
+      "title": "AdonisJs一览",
+      "weight": 0
+    },
+    {
+      "permalink": "contributing",
+      "categories": [
+        "序言"
+      ],
+      "title": "贡献导引",
+      "weight": 0
+    },
+    {
+      "permalink": "release-notes",
+      "categories": [
+        "序言"
+      ],
+      "title": "发行说明",
+      "weight": 0
+    }
+  ],
+  "安全": [
+    {
+      "permalink": "security-introduction",
+      "categories": [
+        "安全"
+      ],
+      "title": "介绍",
+      "weight": 0
+    },
+    {
+      "permalink": "cors",
+      "categories": [
+        "安全"
+      ],
+      "title": "CORS",
+      "weight": 0
+    },
+    {
+      "permalink": "csrf-protection",
+      "categories": [
+        "安全"
+      ],
+      "title": "CSRF保护",
+      "weight": 0
+    },
+    {
+      "permalink": "encryption-and-hashing",
+      "categories": [
+        "安全"
+      ],
+      "title": "加密和哈希",
+      "weight": 0
+    },
+    {
+      "permalink": "shield",
+      "categories": [
+        "安全"
+      ],
+      "title": "Shield中间件",
+      "weight": 0
+    },
+    {
+      "permalink": "data-sanitization",
+      "categories": [
+        "安全"
+      ],
+      "title": "数据过滤",
+      "weight": 0
+    }
+  ],
+  "教程": [
+    {
+      "permalink": "adonis-blog-part1",
+      "categories": [
+        "教程"
+      ],
+      "title": "起步",
+      "weight": 0
+    },
+    {
+      "permalink": "adonis-blog-part2",
+      "categories": [
+        "教程"
+      ],
+      "title": "路由和控制器",
+      "weight": 0
+    },
+    {
+      "permalink": "adonis-blog-part3",
+      "categories": [
+        "教程"
+      ],
+      "title": "数据库模型",
+      "weight": 0
+    },
+    {
+      "permalink": "adonis-blog-part4",
+      "categories": [
+        "教程"
+      ],
+      "title": "显示博客列表",
+      "weight": 0
+    },
+    {
+      "permalink": "adonis-blog-part5",
+      "categories": [
+        "教程"
+      ],
+      "title": "创建博客",
+      "weight": 0
+    },
+    {
+      "permalink": "adonis-blog-part6",
+      "categories": [
+        "教程"
+      ],
+      "title": "整理",
+      "weight": 0
+    }
+  ],
+  "视图": [
+    {
+      "permalink": "view",
+      "categories": [
+        "视图"
+      ],
+      "title": "视图",
+      "weight": 0
+    },
+    {
+      "permalink": "templating",
+      "categories": [
+        "视图"
+      ],
+      "title": "Nunjucks模板",
+      "weight": 0
+    },
+    {
+      "permalink": "form-builder",
+      "categories": [
+        "视图"
+      ],
+      "title": "表单生成器",
+      "weight": 0
+    }
+  ]
+}
